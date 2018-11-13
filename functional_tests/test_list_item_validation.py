4aa192938cb285b37529ea162f1ff1b00c17deda
fernandoarrj <fernandoarrj@gmail.com>
2018-11-13 11:40:49 -0200

    add jquery, qunit tests, lists.js with keypress listeners

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from unittest import skip
 from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
+    
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         # Edith acessa a página inicial e acidentalmente tenta submenter
@@ -58,6 +61,27 @@ class ItemValidationTest(FunctionalTest):
 
         # Ela vê uma mensagem de erro prestativa
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
+        ))
+    
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith inicia uma lista e provoca um erro de validação
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # Ela começa a digitar na caixa de entrada para limpar o erro
+        self.get_item_input_box().send_keys('a')
+    
+        # Ela fica satisfeita ao ver que a mensagem de erro desaparece
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))

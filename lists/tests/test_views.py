88b7e4775a0da63373ef398413ff4e733733b495
fernandoarrj <fernandoarrj@gmail.com>
2018-11-13 10:54:30 -0200

    adjust form to handle duplicate items and save items correct, FT now works complete

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,11 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.utils.html import escape
 
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR, 
+    ExistingListItemForm, ItemForm,
+)
+
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -90,7 +94,7 @@ class ListViewTest(TestCase):
     
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -99,10 +103,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/' + str(list_.id) + '/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
     
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -111,9 +114,9 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
-        self.assertTemplateused(response, 'list.html')
+        self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.count(), 1)
  
 class NewListTest(TestCase):

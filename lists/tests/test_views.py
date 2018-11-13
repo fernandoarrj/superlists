872b009198ecc9359df655c4efa2b008cb61c492
fernandoarrj <fernandoarrj@gmail.com>
2018-11-13 10:38:50 -0200

    adjust form for item and validation duplicate item for same list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from django.urls import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
@@ -100,7 +101,20 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/' + str(list_.id) + '/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+    
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/' + str(list1.id) + '/',
+            data={'text': 'textey'}
+        )
 
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateused(response, 'list.html')
+        self.assertEqual(Item.objects.count(), 1)
  
 class NewListTest(TestCase):
     

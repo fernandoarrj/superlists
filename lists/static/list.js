9a8e1e9d888d8f4dbefae605e2bbc8b73119c5b1
fernandoarrj <fernandoarrj@gmail.com>
2018-11-13 11:45:03 -0200

    adjust script on base.html for holding webpage load to execute scripts

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
-let initialize = function() {
-    $('input[name="text"]').on('keypress', function() {
+window.Superlists = {}
+window.Superlists.initialize = function() {
+    $('input[name="text"]').on('keypress', function () {
         $('.has-error').hide()
     })
 }

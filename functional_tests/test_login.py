bb2bfca9f6ad1658d68f272bc3ab45d4140b7a84
fernandoarrj <fernandoarrj@gmail.com>
2018-11-17 15:44:12 -0200

    FTs now reset database from staging server

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,21 +1,59 @@
+import os
+import poplib
+import re
+import time
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
     
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.gmail.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['GOOGLE_PASSWORD'])
+            while time.time() - start < 60:
+                # obtém as 10 mensagems mais recentes
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+                inbox.quit()
+
     def test_can_get_email_link_to_log_in(self):
         # Edith acessa o incrível site de superlistas
         # e pela primeira vez, percebe que há uma seção de "Log in" na barra
         # de navegação. Essa seção está lhe dizendo para inserir o seu endereço
         # de email, portanto ela faz isso
+        if self.staging_server:
+            test_email = 'souydigital@gmail.com'
+        else:
+            test_email = 'edith@example.com'
+    
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
         
         # Uma mensagem aparece informando-lhe que um email foi enviado
@@ -25,15 +63,13 @@ class LoginTest(FunctionalTest):
         ))
 
         # Ela verifica seu email e encontra uma mensagem
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
         
         # A mensagem contém um link com um url
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.serach(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
         
@@ -41,10 +77,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
         
         # Ela está logada!
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
         
         # Agora ela faz logout
         self.browser.find_element_by_link_text('Log out').click()
 
         # Ela não está mais logada!
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
